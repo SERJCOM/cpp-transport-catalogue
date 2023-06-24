f9d50cd4d539bddf44311aaf5cd3c77775e95c01
SERJCOM <sergeylukin228@yandex.ru>
2023-06-24 17:41:43 +0300

    some changes

--- transport-catalogue/json_builder.cpp
+++ transport-catalogue/json_builder.cpp
@@ -16,80 +16,26 @@ KeyItemContext json::Builder::Key(std::string_view name)
 
 BaseItemContext json::Builder::Value(Node::Value value)
 {   
-    if(CheckArray()){
-        std::get<Array>(nodes_stack_.back()->GetValue()).push_back(value);
-    }
-    else if(CheckDict()){
-        if(!Key_.has_value()){
-            throw std::logic_error("ашипка со словарем");
-        }
-        std::get<Dict>(nodes_stack_.back()->GetValue())[Key_.value()] = value;
-    }
-    else if(CheckNull()) {
+    auto temp = BuildDictArray(value, false);
 
-        nodes_stack_.back()->GetValue() = std::move(value);
-    }
-    else{
-        throw std::logic_error("ашипка с валью");
-    }
-    flag = true;
-
-    Key_.reset();
-
-    return BaseItemContext(*this);
+    return BaseItemContext(*temp);
 }
 
 DictItemContext json::Builder::StartDict()
 {
     Dict temp;
-    if(CheckArray()){
-        std::get<Array>(nodes_stack_.back()->GetValue()).push_back(temp);
+    
+    auto temp_dict = BuildDictArray(temp, true);
 
-        nodes_stack_.push_back(&std::get<Array>(nodes_stack_.back()->GetValue()).back());
-    }
-    else if(CheckDict()){
-        if(!Key_.has_value()){
-            throw std::logic_error("ошибка со словарем");
-        }   
-        nodes_stack_.back()->GetValue() = std::move(temp);
-        nodes_stack_.push_back(nodes_stack_.back());
-    }
-    else if(CheckNull()){
-        nodes_stack_.back()->GetValue() = std::move(temp);
-        nodes_stack_.push_back(nodes_stack_.back());
-    }
-    else{
-        throw std::logic_error("ошибка со словарем");
-    }
-    flag = true;
-     Key_.reset();
-
-    return DictItemContext(*this);
+    return DictItemContext(*temp_dict);
 }
 
 ArrayItemContext json::Builder::StartArray(){
     Array temp;
-    if(Key_.has_value() && CheckDict()){
-        std::get<Dict>(nodes_stack_.back()->GetValue())[Key_.value()] = temp;
-        nodes_stack_.push_back(&std::get<Dict>(nodes_stack_.back()->GetValue()).at(Key_.value()));
-    }
-    else if(CheckArray()){
-        
-        std::get<Array>(nodes_stack_.back()->GetValue()).push_back(temp);
-        nodes_stack_.push_back(nodes_stack_.back());
-    }
-    else if(CheckNull()){
-        nodes_stack_.back()->GetValue() = std::move(temp);
-        nodes_stack_.push_back(nodes_stack_.back());
-    }
-    else{
-        throw std::logic_error("ошибка с массивом");
-    }
-    
-    flag = true;
-    Key_.reset();
 
-    return ArrayItemContext(*this);
+    auto temp_array = BuildDictArray(temp, true);
+
+    return ArrayItemContext(*temp_array);
 }
 
 BaseItemContext json::Builder::EndDict(){
@@ -110,14 +56,16 @@ BaseItemContext json::Builder::EndArray(){
 
 json::Node json::Builder::Build()
 {   
-    if(nodes_stack_.size() != 1 || !flag ) {
+    if(nodes_stack_.size() != 1 || std::holds_alternative<std::nullptr_t>(root_.GetValue())) {
         throw std::logic_error("JSON недостроен");
     }
 
     nodes_stack_.pop_back();
 
-    return root;
+    return root_;
 }
+
+
 
 BaseItemContext BaseItemContext::EndDict(){
     return build.EndDict();
--- transport-catalogue/json_builder.h
+++ transport-catalogue/json_builder.h
@@ -18,7 +18,7 @@ class Builder{
 public:
 
     Builder(){
-        nodes_stack_.push_back(&root);
+        nodes_stack_.push_back(&root_);
     }
 
     KeyItemContext Key(std::string_view name);
@@ -37,28 +37,59 @@ public:
 
 
 private:
-    bool flag = false;
-
+    
     bool CheckDict(){
         return std::holds_alternative<Dict>(nodes_stack_.back()->GetValue());
     }  
-
+    
     bool CheckArray(){
         return std::holds_alternative<Array>(nodes_stack_.back()->GetValue());
     }
-
+    
     bool CheckNull(){
         return std::holds_alternative<std::nullptr_t>(nodes_stack_.back()->GetValue());
     }
 
-    Node root;
+    template<typename T>
+    Builder* BuildDictArray(T element, bool to_stack);
+
+
+    Node root_;
     std::vector<Node*> nodes_stack_;
     std::optional<std::string> Key_;
+
 
 };
 
 
+template<typename T>
+Builder* Builder::BuildDictArray(T element, bool to_stack){
 
+    if(Key_.has_value() && CheckDict()){
+        std::get<Dict>(nodes_stack_.back()->GetValue())[Key_.value()] = std::move(element);
+        
+        if(to_stack)
+            nodes_stack_.push_back(&std::get<Dict>(nodes_stack_.back()->GetValue()).at(Key_.value()));
+    }
+    else if(CheckArray()){
+        
+        std::get<Array>(nodes_stack_.back()->GetValue()).push_back(std::move(element));
+        if(to_stack)
+            nodes_stack_.push_back(nodes_stack_.back());
+    }
+    else if(CheckNull()){
+        nodes_stack_.back()->GetValue() = std::move(element);
+        if(to_stack)
+            nodes_stack_.push_back(nodes_stack_.back());
+    }
+    else{
+        throw std::logic_error("ошибка с массивом или словарем");
+    }
+    
+    Key_.reset();
+
+    return this;
+}
 
 
 
